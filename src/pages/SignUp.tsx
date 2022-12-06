75f13df820889229b172c4175b69d071fec612fa
Sunset-kim <wood2002@naver.com>
2022-12-06 17:35:47 +0900

    feat: signup, signIn

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, FormControl, FormErrorMessage, FormLabel, Input, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, HStack, Input, Stack, Text } from "@chakra-ui/react";
 import { ChangeEvent, FormEvent, SyntheticEvent, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
@@ -8,8 +8,11 @@ const validation = {
   password: (value: string) => value.length >= 8,
 };
 
+type SignMode = "signUp" | "signIn";
+
 function SignUp() {
   const navigate = useNavigate();
+  const [mode, setMode] = useState<SignMode>("signUp");
   const [id, setId] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const { isLoading, isError, data, signIn, signUp, error } = useAuth();
@@ -33,7 +36,7 @@ function SignUp() {
       return;
     }
 
-    signIn({ email: id, password });
+    isSignUp ? signUp({ email: id, password }) : signIn({ email: id, password });
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -47,39 +50,67 @@ function SignUp() {
     }
   };
 
+  const isSignUp = mode === "signUp";
+
   return (
-    <Flex minH="100vh" alignItems="center" justifyContent="center">
-      <Box p="10" shadow="base">
-        <form onSubmit={handleSubmit}>
-          <Stack>
-            <FormControl isInvalid={isIdError}>
-              <FormLabel>Id</FormLabel>
-              <Input type="email" name="id" value={id} onChange={handleChange} autoComplete="username" />
+    <Stack spacing={0} minH="100vh" display="flex" justifyContent="center">
+      <Box>
+        <HStack spacing={0} role={"tablist"}>
+          <Button
+            colorScheme={isSignUp ? "green" : "gray"}
+            variant={isSignUp ? "solid" : "outline"}
+            role={"tab"}
+            borderBottomRadius={0}
+            borderBottom={0}
+            onClick={() => setMode("signUp")}
+          >
+            SignUp
+          </Button>
+          <Button
+            colorScheme={!isSignUp ? "green" : "gray"}
+            variant={!isSignUp ? "solid" : "outline"}
+            role={"tab"}
+            borderBottomRadius={0}
+            borderBottom={0}
+            onClick={() => setMode("signIn")}
+          >
+            SignIn
+          </Button>
+        </HStack>
+        <Box width="100%" alignItems="center" justifyContent="center">
+          <Box p="10" shadow="base">
+            <form onSubmit={handleSubmit}>
+              <Stack>
+                <FormControl isInvalid={isIdError}>
+                  <FormLabel>Id</FormLabel>
+                  <Input type="email" name="id" value={id} onChange={handleChange} autoComplete="username" />
 
-              {isIdError && <FormErrorMessage>E-mail 양식은 반드시 '@'를 포함해야 합니다</FormErrorMessage>}
-            </FormControl>
+                  {isIdError && <FormErrorMessage>E-mail 양식은 반드시 '@'를 포함해야 합니다</FormErrorMessage>}
+                </FormControl>
 
-            <FormControl isInvalid={isPasswordError}>
-              <FormLabel>password</FormLabel>
-              <Input
-                type="password"
-                name="password"
-                value={password}
-                onChange={handleChange}
-                autoComplete="current-password"
-              />
-              {isPasswordError && <FormErrorMessage>Password는 8글자 이상이여야 합니다</FormErrorMessage>}
-            </FormControl>
+                <FormControl isInvalid={isPasswordError}>
+                  <FormLabel>password</FormLabel>
+                  <Input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handleChange}
+                    autoComplete="current-password"
+                  />
+                  {isPasswordError && <FormErrorMessage>Password는 8글자 이상이여야 합니다</FormErrorMessage>}
+                </FormControl>
 
-            {error && <Text color={"red.500"}>{error?.message || "somthing wrong"}</Text>}
+                {error && <Text color={"red.500"}>{error?.message || "somthing wrong"}</Text>}
 
-            <Button disabled={isInValid} type="submit">
-              로그인하기
-            </Button>
-          </Stack>
-        </form>
+                <Button colorScheme="green" disabled={isInValid} type="submit">
+                  로그인하기
+                </Button>
+              </Stack>
+            </form>
+          </Box>
+        </Box>
       </Box>
-    </Flex>
+    </Stack>
   );
 }
 
